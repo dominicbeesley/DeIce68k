1fb966facd1a54a6b2fd5291d2d63d751637ec6b
dominicbeesley <dominic@authorityfile.co.uk>
2021-01-02 19:37:14 +0000

    Moving test data for registers

--- DeIce68k/SampleData/DesignTimeSampleData.cs
+++ DeIce68k/SampleData/DesignTimeSampleData.cs
@@ -45,6 +45,74 @@ namespace DeIce68k.SampleData
                 if (_app == null)
                 {
                     _app = new DeIceAppModel(new DummySerial(), null);
+                    Task.Run(async delegate
+                    {
+                        Random r = new Random();
+                        while (true)
+                        {
+                            await Task.Delay(500);
+                            switch (r.Next(20))
+                            {
+                                case 0:
+                                    _app.Regs.D0.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 1:
+                                    _app.Regs.D1.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 2:
+                                    _app.Regs.D2.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 3:
+                                    _app.Regs.D3.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 4:
+                                    _app.Regs.D4.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 5:
+                                    _app.Regs.D5.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 6:
+                                    _app.Regs.D6.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 7:
+                                    _app.Regs.D7.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 8:
+                                    _app.Regs.A0.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 9:
+                                    _app.Regs.A1.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 10:
+                                    _app.Regs.A2.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 11:
+                                    _app.Regs.A3.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 12:
+                                    _app.Regs.A4.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 13:
+                                    _app.Regs.A5.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 14:
+                                    _app.Regs.A6.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 15:
+                                    _app.Regs.A7u.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 16:
+                                    _app.Regs.A7s.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 17:
+                                    _app.Regs.PC.Data = (uint)r.Next(int.MinValue, int.MaxValue);
+                                    break;
+                                case 19:
+                                    _app.Regs.SR.Data = (uint)r.Next(0xFFFF);
+                                    break;
+                            }
+                        }
+                    });
                 }
                 return _app;
             } 
