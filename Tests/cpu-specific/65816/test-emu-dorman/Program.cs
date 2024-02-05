ce17535bc35c54ddbf6a903db6270eb686b46087
dominicbeesley <dominic@authorityfile.co.uk>
2024-02-05 23:35:10 +0000

    logging optional

--- Tests/cpu-specific/65816/test-emu-dorman/Program.cs
+++ Tests/cpu-specific/65816/test-emu-dorman/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Disass65816;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Transactions;
 
 namespace TestEmuDormann
 {
@@ -15,6 +17,13 @@ namespace TestEmuDormann
 
             wr.WriteLine("""
 TestEmuDormann [options] <binary>
+
+Options:
+--log <log> log to output
+-lw         log writes
+-lr         log reads
+
+Description:
 
 Assumes to load binary at offset 0 and run at 400 with registers
 set as:
@@ -49,7 +58,44 @@ All flags = Unknown
 
         static int Main(string[] args)
         {
-            if (args.Length < 1)
+            bool logreads = false;
+            bool logwrites = false;
+            TextWriter log = null;
+
+            int i = 0;
+            var nextarg = () =>
+            {
+                if (i >= args.Length - 1)
+                    Usage(Console.Error, "Too few arguments", -1);
+
+                return args[i++];
+            };
+
+            while (args[i].StartsWith("-"))
+            {
+                var sw = args[i++];
+
+                if (sw == "--log")
+                {
+                    var fn = nextarg();
+                    try
+                    {
+                        log = new StreamWriter(fn);
+                    } catch (Exception ex)
+                    {
+                        Usage(Console.Error, $"Cannot open log file \"{fn}\" for output", -1, ex);
+                    }
+                } else if (sw == "-lw")
+                {
+                    logwrites = true;
+                }
+                else if (sw == "-lr")
+                {
+                    logreads = true;
+                }
+            }
+
+            if (args.Length < i + 1)
                 Usage(Console.Error, "Wrong number of arguments", 100);
 
             Array.Fill(memory, -1);
@@ -59,7 +105,7 @@ All flags = Unknown
             FileStream fsbin = null;
             try
             {
-                fsbin = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+                fsbin = new FileStream(args[i], FileMode.Open, FileAccess.Read);
             } catch (Exception ex)
             {
                 Usage(Console.Error, $"Cannot open {args[0]} for input", -1, ex);
@@ -72,7 +118,7 @@ All flags = Unknown
                 {
                     Usage(Console.Error, "Empty binary file", -2);
                 }
-                int i = 0;
+                i = 0;
                 while (i < len)
                 {
                     memory[offset++] = buf[i++];
@@ -81,8 +127,17 @@ All flags = Unknown
 
             em65816 em = new em65816()
             {
-                memory_write = (ea, val) => { memory[ea & 0xFFFF] = val; },
-                memory_read = (ea) => { return memory[ea & 0xFFFF]; }
+                memory_write = (ea, val) => { 
+                    memory[ea & 0xFFFF] = val; 
+                    if (logwrites) 
+                        log?.WriteLine($"WR:{ea:X4}<={val:X0}"); 
+                },
+                memory_read = (ea) => { 
+                    var val = memory[ea & 0xFFFF]; 
+                    if (logreads)
+                        log?.WriteLine($"RD:{ea:X4}=>{val:X0}"); 
+                    return val; 
+                }
             };
             var regs = new em65816.Registers(em);
             regs.E = em65816.Tristate.True;
@@ -114,19 +169,19 @@ All flags = Unknown
                     using (var ms = new MemoryStream(pdata))
                     using (var br = new BinaryReader(ms))
                     {
-                        Console.Write($"{regs.PC:X4} ");
+                        log?.Write($"{regs.PC:X4} ");
 
                         var dis = disass.Decode(br, new Address65816_abs((uint)regs.PC));
 
-                        for (int i = 0; i < dis.Length; i++) 
-                            Console.Write($"{pdata[i]:X02} ");
-                        for (int i = dis.Length; i < 4; i++)
-                            Console.Write("   ");
+                        for (i = 0; i < dis.Length; i++) 
+                            log?.Write($"{pdata[i]:X02} ");
+                        for (i = dis.Length; i < 4; i++)
+                            log?.Write("   ");
 
-                        Console.Write(dis.ToString());
+                        log?.Write(dis.ToString());
                     }
 
-                    Console.WriteLine();
+                    log?.WriteLine();
 
                     em65816.instruction_t instr;
                     var regsNext = em.em_65816_emulate(pdata, regs, out instr);
@@ -141,20 +196,22 @@ All flags = Unknown
                         Console.Error.WriteLine($"Stuck at PC={regs.PC:X4} - end of Dormann tests");
                         return -6;
                     }
-                    Console.Write(regsN0.ToString());
+                    log?.Write(regsN0.ToString());
 
                     regs = regsN0;
 
                     instructions++;
-                    if (instructions > 10000000)
-                        done = true;
+//                    if (instructions > 100000000)
+//                        done = true;
+                    if ((instructions & 0xFFFFF) == 0)
+                        Console.WriteLine(".");
                 } 
                 else
                 {
                     Console.Error.WriteLine($"END: Bad program data at {regs.PC:X4}");
                     return -3;
                 }
-                Console.WriteLine();
+                log?.WriteLine();
             } while (!done);
 
 
