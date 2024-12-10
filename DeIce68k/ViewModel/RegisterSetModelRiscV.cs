9ec7b3d05592feea0ce50422368fb156f6c24682
dominicbeesley <dominicbeesley@gmail.com>
2024-12-10 00:12:33 +0000

    predict next instruction

--- DeIce68k/ViewModel/RegisterSetModelRiscV.cs
+++ DeIce68k/ViewModel/RegisterSetModelRiscV.cs
@@ -7,13 +7,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DeIce68k.ViewModel
 {
-    public class RegisterSetModelRiscV : RegisterSetModelBase
+    public class RegisterSetModelRiscV : RegisterSetModelBase, IRegisterSetPredictNext
     {
         public RegisterModel X1 { get; }
         public RegisterModel X2 { get; }
@@ -66,6 +69,7 @@ namespace DeIce68k.ViewModel
         }
 
         public override IDisassState DisassState => new DisassStateArm { };
+
 
         public RegisterSetModelRiscV(DeIceAppModel _parent)
         {
@@ -215,5 +219,249 @@ namespace DeIce68k.ViewModel
 
             return false;
         }
+
+        /* Instruction prediction - move to disassembler? */
+
+        private RegisterModel RegisterAt(uint index)
+        {
+            switch (index)
+            {
+                case 1: return X1;
+                case 2: return X2;
+                case 3: return X3;
+                case 4: return X4;
+                case 5: return X5;
+                case 6: return X6;
+                case 7: return X7;
+                case 8: return X8;
+                case 9: return X9;
+                case 10: return X10;
+                case 11: return X11;
+                case 12: return X12;
+                case 13: return X13;
+                case 14: return X14;
+                case 15: return X15;
+                case 16: return X16;
+
+                case 17: return X17;
+                case 18: return X18;
+                case 19: return X19;
+                case 20: return X20;
+                case 21: return X21;
+                case 22: return X22;
+                case 23: return X23;
+                case 24: return X24;
+                case 25: return X25;
+                case 26: return X26;
+                case 27: return X27;
+                case 28: return X28;
+                case 29: return X29;
+                case 30: return X30;
+                case 31: return X31;
+                default:
+                    throw new ArgumentException($"Index must be 32<n<0 {index}");
+            }
+
+        }
+
+        private UInt32 RegisterVal(uint index)
+        {
+            if (index == 0)
+                return 0;
+            else
+                return RegisterAt(index).Data;
+        }
+            
+
+        private RegisterModel CRegisterAt(uint index)
+        {
+            switch (index)
+            {
+                case 0: return X8;
+                case 1: return X9;
+                case 2: return X10;
+                case 3: return X11;
+                case 4: return X12;
+                case 5: return X13;
+                case 6: return X14;
+                case 7: return X15;
+                default:
+                    throw new ArgumentException($"Index must be 32<n<0 {index}");
+            }
+        }
+
+        private UInt32 CRegisterVal(uint index)
+        {
+            if (index == 0)
+                return 0;
+            else
+                return CRegisterAt(index).Data;
+        }
+
+
+        private static long Signed(uint i, int nbits)
+        {
+            int sb = 1 << (nbits - 1);
+            uint m = 0xFFFFFFFF << nbits;
+            return (int)(((i & sb) != 0) ? (m | i) : i);
+
+        }
+
+
+        public DisassAddressBase PredictNext(byte[] programdata)
+        {
+
+            DisassAddressBase ret;
+            if ((programdata[0] & 0b11) == 0b11)
+            {
+                //TODO: >32 bits?
+                ret = PCValue + 4;
+
+                uint instr = (uint)(
+                    ((UInt32)programdata[0]) 
+                    | ((UInt32)programdata[1] << 8) 
+                    | ((UInt32)programdata[2] << 16) 
+                    | ((UInt32)programdata[3] << 24));
+
+                if ((instr & 0b1111111) == 0b1100011)
+                {
+                    // branches
+                    uint offs = (
+                                ((instr & 0x80000000) >> 19) | // 12
+                                ((instr & 0x7E000000) >> 20) | // 10:5
+                                ((instr & 0x00000F00) >> 7) | // 4:1
+                                ((instr & 0x00000080) << 4) // 11
+                            );
+                    uint rs2 = (instr & 0x01F00000) >> 20;
+                    uint rs1 = (instr & 0x000F8000) >> 15;
+                    uint f3 = (instr & 0x00007000) >> 12;
+                    bool branch = f3 switch
+                    {
+                        //beq
+                        0 => RegisterVal(rs1) == RegisterVal(rs2),
+                        //bne
+                        1 => RegisterVal(rs1) != RegisterVal(rs2),
+                        //blt
+                        4 => Signed(RegisterVal(rs1), 32) < Signed(RegisterVal(rs2), 32),
+                        //bge
+                        5 => Signed(RegisterVal(rs1), 32) >= Signed(RegisterVal(rs2), 32),
+                        //bltu
+                        6 => RegisterVal(rs1) < RegisterVal(rs2),
+                        //bgeu
+                        7 => RegisterVal(rs1) >= RegisterVal(rs2),
+                        _ => false
+                    };
+                    if (branch)
+                    {
+                        ret = PCValue + Signed(offs, 12);
+                    }
+                } else if ((instr & 0b1111111) == 0b1101111)
+                {
+                    // jump
+                    uint imm = (
+                                ((instr & 0x80000000) >> 11) | // 20
+                                ((instr & 0x7FE00000) >> 20) | // 10:1
+                                ((instr & 0x00100000) >> 9) | // 11
+                                ((instr & 0x000FF000)) // 19:12
+                            );
+
+                    ret = PCValue + Signed(imm, 20);                    
+                } else if ((instr & 0b1111111) == 0b1100111)
+                {
+                    uint imm = (instr & 0xFFF00000) >> 20;
+                    uint rs1 = (instr & 0x000F8000) >> 15;
+                    uint f3 = (instr & 0x00007000) >> 12;
+                    uint rd = (instr & 0x00000F80) >> 7;
+
+                    if (f3 == 0)
+                    {
+                        return new AddressRiscV((uint)(RegisterAt(rs1).Data + Signed(imm, 12)) & 0xFFFFFFFE);
+                    }
+
+                }
+            }
+            else
+            {
+                // a 16 bit instruction
+                ret = PCValue + 2;
+                uint instr = (uint)(programdata[0] | (programdata[1] << 8));
+                //check for jumps/branches
+                if (
+                    ((instr & 0b1110_0000_0000_0011) == 0b0010_0000_0000_0001)
+                    || ((instr & 0b1110_0000_0000_0011) == 0b1010_0000_0000_0001)
+                ) {
+                    uint offs = (uint)(
+                   ((instr & 0x1000) >> 1)  // 12   -> 11
+                 | ((instr & 0x0800) >> 7)  // 11   -> 4
+                 | ((instr & 0x0600) >> 1)  // 10:9 -> 9:8
+                 | ((instr & 0x0100) << 2)  // 8    -> 10
+                 | ((instr & 0x0080) >> 1)  // 7    -> 6
+                 | ((instr & 0x0040) << 1)  // 6    -> 7
+                 | ((instr & 0x0038) >> 2)  // 5:3  -> 3:1
+                 | ((instr & 0x0004) << 3)  // 2    -> 5
+                 );
+
+                    //CJ types
+                    ret = PCValue + Signed(offs, 12);
+                }
+                else if ((instr & 0b1110_0000_0000_0011) == 0b1000_0000_0000_0010)
+                {
+                    //CR types
+                    uint f4 = (uint)((instr & 0xF000) >> 12);
+                    uint rdrs1 = (uint)((instr & 0x0F80) >> 7);
+                    uint rs2 = (uint)((instr & 0x007C) >> 2);
+
+                    if (f4 == 0b1000 && rs2 == 0)
+                    {
+                        //c.ret / c.jr
+                        if (rdrs1 == 0)
+                            ret = new AddressRiscV(0);
+                        else
+                            ret = new AddressRiscV(RegisterAt(rdrs1).Data);
+                    }
+                    else if (f4 == 0b1001 && rs2 == 0)
+                    {
+                        if (rdrs1 == 0 && rs2 == 0)
+                            ret = PCValue; // c.ebreak
+                        else if (rs2 == 0)
+                            ret = new AddressRiscV(RegisterAt(rdrs1).Data);
+                    }
+                }
+                else if ((instr & 0b1100_0000_0000_0011) == 0b1100_0000_0000_0001)
+                {
+                    uint imm = (uint)(
+                            ((instr & 0x1000) >> 4)
+                            | ((instr & 0x0C00) >> 7)
+                            | ((instr & 0x0060) << 1)
+                            | ((instr & 0x0018) >> 2)
+                            | ((instr & 0x0004) << 3)
+                            );
+                    uint rs_ = (uint)((instr & 0x0380) >> 7);
+
+                    //c.b??
+                    if ((instr & 0x2000) == 0)
+                    {
+                        //c.beqz
+                        if (CRegisterVal(rs_) == 0)
+                            ret = PCValue + Signed(imm, 9);
+                    }
+                    else
+                    {
+                        //c.bnez
+                        if (CRegisterVal(rs_) != 0)
+                            ret = PCValue + Signed(imm, 9);
+                    }
+                }
+
+            }
+
+            Parent.AppendMessage($":PRED:{PCValue.Canonical:X8} -> {ret.Canonical:X8}");
+            Parent.AppendMessage(new System.Diagnostics.StackTrace().ToString());
+
+            return ret;
+        }
+
+        public int PredictProgramDataSize => 4;
+
     }
 }
